1554d1e1f699396133eae85873fc5d12d2a62292
jcagz <jcagz@iscte-iul.pt>
2018-10-10 20:40:06 +0100

    Adicionada classe Facebook

---
+++ JavaFX/src/RedesSociais/Facebook.java
@@ -0,0 +1,24 @@
+package RedesSociais;
+
+//import java.util.List;
+
+public class Facebook {  //implements interfaceFiltragem
+	
+	
+	
+	//private List<Post> posts;
+	
+	public Facebook() {
+		
+	}
+	
+	
+	
+
+	
+	
+	public static void main(String[] args) {
+
+	}
+
+}
