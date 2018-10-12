ec1f54e71ca648adb07cb0244968b6a49b7cb7ff
jcagz <jcagz@iscte-iul.pt>
2018-10-12 15:10:37 +0100

    adicionado integração dos posts do facebook com a GUI

--- JavaFX/src/RedesSociais/Facebook.java
+++ JavaFX/src/RedesSociais/Facebook.java
@@ -1,24 +1,51 @@
 package RedesSociais;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.restfb.Connection;
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.types.Page;
+import com.restfb.types.Post;
+import com.restfb.types.User;
+
 //import java.util.List;
 
 public class Facebook {  //implements interfaceFiltragem
+	
+	private final String accessToken = "EAAfIbZAN045UBAGZB4i8TxPzMZAwvjDS7WqrUAk1ZCteeZBmcwBTksKH2gjmI5OaAeqZCx6bfr3UGSWSWAbjIINVN7CtnD7La4ZA4do49Ieye3lZAk0EfQtmI8J2qXzVZCgZA4hazBHgtmV7LRlqHRlUayc95P2j992H3oFFgAZAxlAip8AdSpMa4fF";
+	private FacebookClient fbClient = new DefaultFacebookClient(accessToken);
+	private final User me = fbClient.fetchObject("me", User.class);
+	private List<Post> posts = new ArrayList<Post>();
+	
 	
 	
 	
 	//private List<Post> posts;
 	
 	public Facebook() {
+		addPostsToArray();
+	}
+	
+	
+	public void addPostsToArray() {
+		Connection<Post> result = fbClient.fetchConnection("me/feed",Post.class);
+		
+		for(List<Post> page: result) {
+			for(Post aPost : page) {
+				posts.add(aPost);
+			}
+		}
+		
 		
 	}
 	
 	
-	
-
-	
-	
-	public static void main(String[] args) {
-
+	public List<Post> getPosts(){
+		return posts;
 	}
+	
+	
 
 }
--- JavaFX/src/application/Main_Controller.java
+++ JavaFX/src/application/Main_Controller.java
@@ -1,22 +1,66 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.restfb.types.Post;
+
+import RedesSociais.Facebook;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
 
 public class Main_Controller {
 
+	
+	private Facebook fb;
+	private ArrayList<Post> p;
+	
     @FXML
     private TextArea area;
 
     @FXML
     private Button botao;
+    
+    @FXML
+    private ListView<String> lista;
+    
+    @FXML
+    private Tab tab1;
+    
+    @FXML
+    private TabPane tabPane;
+    
+    
+    public Main_Controller() {
+		fb = new Facebook();
+		p =(ArrayList<Post>) fb.getPosts();
+		
+	}
+    
+    
+    
 
     @FXML
     void printa(ActionEvent event) {
     	System.out.println("printa");
     	area.appendText("benfica");
+    	
+    	for(Post pl : p) {
+			lista.getItems().add(pl.getMessage());
+		}
+    	
     }
+    
+    
+    
+    
+   
 
 }
