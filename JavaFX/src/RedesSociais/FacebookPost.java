fdbc59e5faf376c3a0e67405143cde0113fe453f
jcagz <jcagz@iscte-iul.pt>
2018-10-13 09:14:19 +0100

    Adicionada classe FacebookPost

---
+++ JavaFX/src/RedesSociais/FacebookPost.java
@@ -0,0 +1,33 @@
+package RedesSociais;
+
+import java.util.Date;
+
+import com.restfb.types.Post;
+
+public class FacebookPost {
+
+	
+	private String postPreview;
+	private Date date;
+	private String fullPost;
+	
+	public FacebookPost(Post post) {
+		this.postPreview = post.getCreatedTime().toString() + " - " + post.getMessage().substring(0, 22);
+		this.date = post.getCreatedTime();
+		this.fullPost = post.getMessage();
+	}
+		
+	public String getPostPreview() {
+		return postPreview;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public String getFullPost() {
+		return fullPost;
+	}
+
+
+}
