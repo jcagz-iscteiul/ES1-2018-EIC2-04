42269fd8fcbc3fb2eb3b86859027f9df039d6c29
acmon-iscteiul <acmon@iscte-iul.pt>
2018-11-22 23:42:53 +0000

    Adicionado comentario na classe EmailPost

--- JavaFX/src/RedesSociais/EmailPost.java
+++ JavaFX/src/RedesSociais/EmailPost.java
@@ -16,34 +16,52 @@ public class EmailPost extends PostGeral{
 		this.to = to;
 	}
 
-	
+	/**
+	 * Retorna o atributo da superclasse titulo
+	 * @return String
+	 */
 	public String getAssunto() {
 		return titulo;
 	}
 
-
+	/**
+	 * Retorna o atributo da superclasse data
+	 * @return Date
+	 */
 	public Date getData() {
 		return super.getData();
 	}
 
-
+	/**
+	 * Retorna o atributo da superclasse conteudo
+	 * @return String
+	 */
 	public String getConteudo() {
 		return conteudo;
 	}
 
-
+	/**
+	 * Retorna o atributo from
+	 * @return String
+	 */
 	public String getFrom() {
 		return from;
 	}
 
 
-
+	/**
+	 * Retorna o atributo to
+	 * @return String
+	 */
 	public String getTo() {
 		return to;
 	}
 
 
-
+	/**
+	 * Retorna uma string concatenado com a data do email mais o titulo
+	 * @return String
+	 */
 	public String emailPostPreview() {
 		String str = data.toString() + " - " + titulo;
 		
