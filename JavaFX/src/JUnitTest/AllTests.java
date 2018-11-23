5c98d90a5ec3e8e6a3c9730ffa858db7be6a6ed4
acmon-iscteiul <acmon@iscte-iul.pt>
2018-11-23 12:48:27 +0000

    Foi gerado javadoc e AllTests foi atualizado

--- JavaFX/src/JUnitTest/AllTests.java
+++ JavaFX/src/JUnitTest/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ FacebookTest.class, GmailTest.class, testeXML.class })
+@SuiteClasses({ FacebookTest.class, GmailTest.class, testeXML.class, TwitterTest.class })
 public class AllTests {
 
 }
