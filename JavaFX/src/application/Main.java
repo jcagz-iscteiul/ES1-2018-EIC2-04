f7a8a82210ede29a9b342814b74cafb80be2b28e
Francisco <fmoaa@iscte-iul.pt>
2018-12-03 15:25:45 +0000

    Icon adicionado (ES)

--- JavaFX/src/application/Main.java
+++ JavaFX/src/application/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 
 
@@ -22,9 +23,13 @@ public class Main extends Application {
 			primaryStage.setOnHidden(e -> controller.shutDown());
 		}
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		Image icon = new Image(getClass().getResourceAsStream("/images/ES.png"));
+		primaryStage.getIcons().add(icon);
 		primaryStage.setTitle("BDA");
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
+		
 	}
 	public static void main(String[] args) {
 		launch(args);	
