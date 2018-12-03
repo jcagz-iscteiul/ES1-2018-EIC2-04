4f37cfa2a1d8eb32751bec826c5cfce0e0616498
acmon-iscteiulpt <acmon@iscte-iul.pt>
2018-12-03 21:05:24 +0000

    Adicionei BaseDadosTest no ficheiro AllTests

--- JavaFX/src/JUnitTest/AllTests.java
+++ JavaFX/src/JUnitTest/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ DestaquesTest.class, EmailPostTest.class, FacebookPostTest.class ,FacebookTest.class, GmailTest.class, testeXML.class, 
+@SuiteClasses({BaseDadosTest.class, DestaquesTest.class, EmailPostTest.class, FacebookPostTest.class ,FacebookTest.class, GmailTest.class, testeXML.class, 
 	TwitterPostTest.class, TwitterTest.class })
 public class AllTests {
 
