8e31f2f451ad67364295cadcb473789324022f17
acmon-iscteiulpt <acmon@iscte-iul.pt>
2018-12-03 12:54:37 +0000

    Teste JUnit da classe Destaques - Incompleto

---
+++ JavaFX/src/JUnitTest/DestaquesTest.java
@@ -0,0 +1,43 @@
+package JUnitTest;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import RedesSociais.Destaques;
+import RedesSociais.Facebook;
+import RedesSociais.Gmail;
+import RedesSociais.PostGeral;
+import RedesSociais.TwitterObject;
+
+public class DestaquesTest {
+	
+	private Destaques destaques;
+	
+	@Before
+	public void setUp() {
+		Gmail gm = new Gmail();
+		Facebook fb = new Facebook();
+		TwitterObject tw = new TwitterObject();
+		this.destaques = new Destaques(gm.getLista_posts(), fb.getLista_posts(), tw.getLista_posts());
+	}
+	
+	@Test
+	public void test_getDestaques() {
+		for(PostGeral post: destaques.getDestaques()) {
+			assertNotNull(post);
+		}
+	}
+	
+	@Test
+	public void test_setDestaques() {
+		
+	}
+	
+	
+	
+	
+}
